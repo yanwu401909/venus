c6b320ccc91b12c5d6a920f30600dd00770a8307
nazul <yanwu401909@gmail.com>
2019-03-10 12:51:41 +0800

    modify images url

--- lib/mock/mock.dart
+++ lib/mock/mock.dart
@@ -8,7 +8,7 @@ var pictureCardList = [
     follows: 67,
     following: 56,
     imageUrl:
-        "https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1552086791&di=2bcbb1d38f2161b89237ff5b30aea577&src=http://img2.ph.126.net/XnbYnlmIp1DaNpFwIvmOiA==/3026137474716469262.jpg",
+        "http://img2.ph.126.net/XnbYnlmIp1DaNpFwIvmOiA==/3026137474716469262.jpg",
   ),
   PictureCardModel(
     name: "Dixie Schowalter",
@@ -17,7 +17,7 @@ var pictureCardList = [
     follows: 125,
     following: 70,
     imageUrl:
-        "https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1552087193&di=1dbcef8508a06ce1e3338c4829556fa9&src=http://img1.ph.126.net/CZca-luk68BJZdaRGHnD2Q==/6597316057263097882.jpg",
+        "http://img1.ph.126.net/CZca-luk68BJZdaRGHnD2Q==/6597316057263097882.jpg",
   ),
   PictureCardModel(
     name: "Clementina Lowe",
@@ -26,7 +26,7 @@ var pictureCardList = [
     follows: 97,
     following: 78,
     imageUrl:
-        "https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1552087193&di=53e471322341895c1054a0d75a17ea38&src=http://xnnews.com.cn/wenyu/lxsj/201801/W020180109419382927916.jpg",
+        "http://xnnews.com.cn/wenyu/lxsj/201801/W020180109419382927916.jpg",
   ),
   PictureCardModel(
     name: "Jaime Abbott",
@@ -35,7 +35,7 @@ var pictureCardList = [
     follows: 234,
     following: 143,
     imageUrl:
-        "https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1552087193&di=c6761fff84f473e59fa63150125a52bb&src=http://img0.ph.126.net/AYV8T0EPz9ncKkDt3onozQ==/2433914123635648452.jpg",
+        "http://img0.ph.126.net/AYV8T0EPz9ncKkDt3onozQ==/2433914123635648452.jpg",
   ),
   PictureCardModel(
     name: "Laurie",
@@ -89,6 +89,6 @@ var pictureCardList = [
     follows: 15893,
     following: 42185,
     imageUrl:
-        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552096857979&di=bd9b1c31103de9385f413d7fa716afc8&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2Fk5GERDqGWWGPlcNb_S2nHA%3D%3D%2F2190156793885905479.jpg",
+        "http://img1.ph.126.net/XrtcAgyFQk7y0T71Bks4lQ==/6608606942166519583.jpg",
   ),
 ];
